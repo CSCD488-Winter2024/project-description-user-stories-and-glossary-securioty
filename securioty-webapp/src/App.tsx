f810107012b734ccd8c735a80e61bd13ad3e0773
Ben Knoll <bknoll@ewu.edu>
2024-03-07 13:47:02 -0800

    Initial Files and Router
    * Added components LabCard, LabSet, NavBar
    * Added initial pages Home, Lab, Profile
    * Setup React Router to route to each page based on the NavBar
    * Setup dependencies for bootstrap/router

--- securioty-webapp/src/App.tsx
+++ securioty-webapp/src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Lab from "./pages/Lab";
+import Profile from "./pages/Profile";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <NavBar></NavBar>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/lab" element={<Lab />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ securioty-webapp/src/components/NavBar.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand ms-3" href="/home">
+            SecurIoTy
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav mb-2 mb-lg-0">
+              <li className="nav-item ml-auto">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="/profile"
+                >
+                  Profile
+                </a>
+              </li>
+              <li className="nav-item me-3">
+                <a className="nav-link" href="#">
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default NavBar;
