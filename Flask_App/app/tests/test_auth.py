a86f971907f054f3d781950674299185d0376e75
Caleb Steinman <csteinman@ewu.edu>
2024-04-10 14:07:52 -0700

    Additional Unit Tests & Dependency added

--- Flask_App/app/tests/test_auth.py
+++ Flask_App/app/tests/test_auth.py
@@ -7,7 +7,7 @@ from ..auth.models import User
 
 @pytest.fixture
 def client():
-    """Creates app in testing mode, and creates a mock db that self destructs after usage"""
+    """Creates app in testing mode, and creates a mock db that self-destructs after usage"""
     app = create_app('testing')
 
     with app.test_client() as client:
@@ -19,7 +19,7 @@ def client():
             db.drop_all()
 
 
-def test_registration(client):
+def test_registration_success(client):
     """Using mocked database in memory, tests valid responses from registration"""
     response = client.post('/auth/register', json={
         'email': 'test@example.com',
@@ -34,7 +34,7 @@ def test_registration(client):
     assert User.query.count() == 1
 
 
-def test_login(client):
+def test_login_success(client):
     """Using mocked database in memory, tests valid response from login"""
     client.post('/auth/register', json={
         'email': 'test@example.com',
@@ -52,4 +52,32 @@ def test_login(client):
     login_data = login_response.get_json()
     assert login_response.status_code == 200
     assert 'access_token' in login_data
-    assert User.query.count() == 1
+
+
+def test_login_failure(client):
+    """Using mocked database in memory, tests invalid response from login"""
+    client.post('/auth/register', json={
+        'email': 'test@example.com',
+        'password': 'testPassword',
+        'first': 'testFirst',
+        'last': 'testLast',
+        'role': 'STUDENT'
+    })
+
+    login_response = client.post('auth/login', json={
+        'email': 'test@example.com',
+        'password': 'INCORRECT'
+    })
+
+    login_data = login_response.get_json()
+    assert login_response.status_code == 401
+    assert login_data['message'] == 'Invalid email or password'
+
+    login_response = client.post('auth/login', json={
+        'email': 'INVALID_USER',
+        'password': 'FAKE_PASSWORD'
+    })
+
+    login_data = login_response.get_json()
+    assert login_response.status_code == 401
+    assert login_data['message'] == 'Invalid email or password'
