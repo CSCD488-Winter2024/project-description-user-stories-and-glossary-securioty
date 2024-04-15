a883dc1a212ac780863e51ff21c7838e469faf53
John "JT" Taylor <jtaylor37@ewu.edu>
2024-04-15 16:06:22 -0700

    Update AnswerBox
    added functionality to the submit button in the answerbox component

--- securioty-webapp/src/components/AnswerBox.tsx
+++ securioty-webapp/src/components/AnswerBox.tsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 
 const AnswerBox = () => {
+  const [answer, setAnswer] = useState("")
+  const [answerFeedback, setAnswerFeedback] = useState("")
+
+  function handleAnswer(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    
+    if (answer === "flag") {
+      setAnswerFeedback("Correct!");
+    } else {
+      setAnswerFeedback("Incorrect. Try again.");
+    }
+}
+
+
   return (
-    <form>
+    <form onSubmit={handleAnswer}>
       <div className="mb-3">
         <label className="form-label">Answer</label>
         <input
+          value = {answer}
+          onChange={e => setAnswer(e.target.value)}
           type="text"
           className="form-control"
-          id="exampleInputPassword1"
+          id="answer1"
+          name="answer"
         />
+        {answerFeedback !== null && <div>{answerFeedback}</div>}
       </div>
       <button type="submit" className="bg-primary btn btn-primary">
         Submit
+        
       </button>
     </form>
   );
