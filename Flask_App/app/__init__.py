f110c424b273631710e02900955bdd40adc57d77
Caleb Steinman <csteinman@ewu.edu>
2024-04-18 17:36:24 -0700

    Add test config for FT

--- Flask_App/app/__init__.py
+++ Flask_App/app/__init__.py
@@ -1,13 +1,9 @@
 """Creates the Flask app with all necessary configurations and blueprints"""
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+from .extensions import db, bcrypt, jwt
 from ..config import config_dict
-
-db = SQLAlchemy()
-bcrypt = Bcrypt()
-jwt = JWTManager()
+from .auth.models import User
 
 
 def create_app(config_name):
@@ -18,7 +14,17 @@ def create_app(config_name):
     bcrypt.init_app(app)
     jwt.init_app(app)
 
+    if config_name == 'testing':
+        with app.app_context():
+            db.create_all()
+            if User.query.count() == 0:
+                dummy_user = User(Email='Test@gmail.com', First='John', Last='Doe', Role='ADMIN')
+                dummy_user.set_password('TEST1234')
+                db.session.add(dummy_user)
+                db.session.commit()
+
     from .auth.routes import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
+    CORS(app)
     return app
--- Flask_App/run.py
+++ Flask_App/run.py
@@ -1,6 +1,6 @@
 """Driver file which creates the app based on passed in configs"""
 import os
-from app import create_app
+from .app import create_app
 
 # Defaults to dev if not given specific config
 config_name = os.getenv('FLASK_CONFIG', 'development')
