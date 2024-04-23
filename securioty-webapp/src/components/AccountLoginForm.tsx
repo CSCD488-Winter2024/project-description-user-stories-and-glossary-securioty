381a00b6796b175e79bd65868d1870074912e984
Ben Knoll <bknoll@ewu.edu>
2024-04-23 12:47:42 -0700

    Removed unused imports

--- securioty-webapp/src/components/AccountLoginForm.tsx
+++ securioty-webapp/src/components/AccountLoginForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import accountData from "../scripts/accountData";
 import axios from "axios";
 
--- securioty-webapp/src/components/NavBar.tsx
+++ securioty-webapp/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import AccountPopup from "./AccountLoginForm";
 import LoginButton from "./LoginButton";
 import accountData from "../scripts/accountData";
